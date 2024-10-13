bec0ed0ad6fb8b4e0bcc7c6c530e9224ef817f00
RAHMAT SANG JAYA GULO <150424189+rahmatgulo@users.noreply.github.com>
2024-10-13 10:47:24 +0700

    Pertemuan 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$nama = "Rahmat Sang Jaya Gulo";
+$umur = "18";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contoh Variabel PHP</title>
+</head>
+<body>
+<h1>Halo, <?php echo $nama; ?>!</h1>
+<p>Umur Anda adalah <?php echo $umur; ?> tahun.</p>
+</body>
+</html>
